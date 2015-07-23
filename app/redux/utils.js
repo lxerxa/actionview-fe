cf89326f84085bd458b959f510f7916ee3b07ca4
iam4x <iam4x@me.com>
2015-07-23 02:00:32 +0200

    refactor: redux/utils

--- app/redux/utils.js
+++ app/redux/utils.js
@@ -1,22 +1,15 @@
 // Utils for removing boilerplate from Redux
-import types from './constants/ActionTypes';
+import t from './constants/ActionTypes';
 
 export function asyncFuncCreator({ CONSTANT, promise, ...rest }) {
-  return {
-    types: [
-      types[CONSTANT],
-      types[`${CONSTANT}_SUCCESS`],
-      types[`${CONSTANT}_FAIL`]
-    ],
-    promise,
-    ...rest
-  };
+  const types = [t[CONSTANT], t[CONSTANT + '_SUCCESS'], t[CONSTANT + '_FAIL']];
+  return { types, promise, ...rest};
 }
 
 export function generateAsyncConstants(CONSTANT) {
   return {
     [CONSTANT]: CONSTANT,
-    [`${CONSTANT}_SUCCESS`]: `${CONSTANT}_SUCCESS`,
-    [`${CONSTANT}_FAIL`]: `${CONSTANT}_FAIL`
+    [CONSTANT + '_SUCCESS']: CONSTANT + '_SUCCESS',
+    [CONSTANT + '_FAIL']: CONSTANT + '_FAIL'
   };
 }
