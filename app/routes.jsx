abc9517a08a69e8d9b66d38bde1a2cf5cdf6cc01
iam4x <iam4x@me.com>
2015-08-26 00:07:54 +0200

    feat(localized-routes): add utils to generate routes

--- app/routes.jsx
+++ app/routes.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Route } from 'react-router';
 
+import { generateRoute } from 'utils/localized-routes';
+
 export default (
   <Route component={ require('components/Layout') }>
-    <Route path='/' component={ require('components/Users') } />
-    <Route path='/users/:seed' component={ require('components/Profile') } />
-    <Route path='readme' component={ require('components/Readme') } />
+    { generateRoute({
+      paths: [ '/', '/users', '/utilisateurs' ],
+      component: require('components/Users')
+    }) }
+    { generateRoute({
+      paths: [ '/users/:seed', '/utilisateurs/:seed' ],
+      component: require('components/Profile')
+    }) }
+    { generateRoute({
+      paths: [ '/readme', '/lisez-moi' ],
+      component: require('components/Readme')
+    }) }
   </Route>
 );
