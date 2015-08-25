7661b1a5024c7e42e1d0ec71ca9e42fef9641300
iam4x <iam4x@me.com>
2015-08-25 21:27:30 +0200

    feat(navbar): add locale switch

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import cx from 'classnames';
 
+import * as I18nActions from 'redux/actions/I18nActions';
+import { getIntlMessage } from 'utils/react-intl-wrapper';
+
+@connect(({ i18n }) => ({ ...i18n }))
 class Navbar extends Component {
+
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    locale: PropTypes.string,
+    messages: PropTypes.object
+  }
+
+  i18n = getIntlMessage
+  actions = bindActionCreators(I18nActions, this.props.dispatch)
 
   render() {
     return (
@@ -11,14 +27,27 @@ class Navbar extends Component {
             <ul className='nav navbar-nav'>
               <li>
                 <Link to='/'>
-                  User List
+                  { this.i18n('users') }
                 </Link>
               </li>
               <li>
                 <Link to='/readme'>
-                  Readme
+                  { this.i18n('readme') }
                 </Link>
               </li>
+            </ul>
+            <ul className='nav navbar-nav navbar-right'>
+              { ['en', 'fr'].map(locale =>
+                  <li
+                    key={ locale }
+                    className={ cx({ active: this.props.locale === locale }) }>
+                    <a
+                      onClick={ () => this.actions.change(locale) }
+                      className='text-uppercase'>
+                      { locale }
+                    </a>
+                  </li>
+              ) }
             </ul>
           </div>
         </div>
