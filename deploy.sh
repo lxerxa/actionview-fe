23e9ad9d6bebc2bee483a111d62e3fa7b6e0b8ec
lxerxa <lxerxa@126.com>
2018-02-07 23:09:44 +0800

    sider bar sort

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ rm ../public/scripts/app*
 mv ../public/assets/common.js ../public/scripts/common.js
 mv ../public/assets/app*.js ../public/scripts/app.js
 
+rm ../public/assets/*.map
 #mv ../public/assets/app*.js.map ../public/scripts/app.js.map
 
 echo 'deploy complete!'
