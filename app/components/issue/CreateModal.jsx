cd73bc9fac9aed7c01a2336018e5267daf6a4e50
lxerxa <actionview@126.com>
2018-07-30 22:13:07 +0800

    checkbox style

--- app/components/issue/CreateModal.jsx
+++ app/components/issue/CreateModal.jsx
@@ -455,7 +455,7 @@ class CreateModal extends Component {
                       value={ this.state.values[v.key] && _.isString(this.state.values[v.key]) ? this.state.values[v.key].split(',') : this.state.values[v.key] }
                       onChange={ newValue => { v.required && newValue.length <= 0 ? this.state.errors[v.key] = '必选' : delete this.state.errors[v.key]; this.state.touched[v.key] = true; this.state.values[v.key] = newValue; this.setState({ values: this.state.values, errors: this.state.errors, touched: this.state.touched }) } }>
                       { _.map(v.optionValues || [], (val, i) => 
-                        <span key={ i }><Checkbox disabled={ loading } value={ val.id }/>{ ' ' + val.name + ' ' }</span>
+                        <span style={ { marginLeft: '6px' } } key={ i }><Checkbox disabled={ loading } value={ val.id }/>{ ' ' + val.name + ' ' }</span>
                         )
                       }
                       { this.state.touched[v.key] && this.state.errors[v.key] && <div><ControlLabel>{ this.state.errors[v.key] || '' }</ControlLabel></div> }
