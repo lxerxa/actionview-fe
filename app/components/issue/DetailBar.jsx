16397925e089baba65a11de96230eeb4bcdf0bc9
lxerxa <actionview@126.com>
2019-01-20 09:23:11 +0800

    merge master

--- app/components/issue/DetailBar.jsx
+++ app/components/issue/DetailBar.jsx
@@ -777,6 +777,7 @@ export default class DetailBar extends Component {
                   </Col>
                   <Col sm={ 7 }>
                     <div style={ { marginTop: '7px' } }>
+                      <Link to={ '/project/' + project.key + '/issue?epic=' + data.epic }>
                         <span className='epic-title' style={ { borderColor: selectedEpic.bgColor, backgroundColor: selectedEpic.bgColor, maxWidth: '100%', marginRight: '5px', marginTop: '0px', float: 'left' } } title={ selectedEpic.name || '-' } >
                           { selectedEpic.name || '-' }
                         </span>
