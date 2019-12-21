bfdbef4f787cbc40ea69970ebc7c5e72006e8f52
lxerxa <actionview@126.com>
2019-12-21 17:27:19 +0800

    add jquery-ui

--- app/app.js
+++ app/app.js
@@ -25,10 +25,9 @@ if (BROWSER) {
   require('assets/styles/board.css');
   require('assets/styles/calendar.css');
   require('assets/styles/jquery.atwho.css');
+  require('assets/styles/jquery-ui.css');
   require('mermaid/dist/mermaid.css');
   require('react-dropzone-component/styles/filepicker.css');
-  require('dropzone/dist/dropzone.css');
-  require('cropperjs/dist/cropper.css');
 }
 
 (async function() {
