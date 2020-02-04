1d934cf94ea71810c6afc78c31288bed50a9b798
lxerxa <lxerxa@126.com>
2020-02-04 08:30:59 +0800

    error path

--- app/components/layout/Error.jsx
+++ app/components/layout/Error.jsx
@@ -13,7 +13,7 @@ class Error extends Component {
         if (second < 0) {
           clearInterval(this.timer);
           this.timer = undefined;
-          this.context.router.push({ pathname: '/myproject' });
+          this.context.router.push({ pathname: '/login' });
         } else {
           document.getElementById('num').innerHTML = second;
         }
@@ -22,11 +22,14 @@ class Error extends Component {
 
   render() {
     return (
-      <div style={ { paddingTop: '180px', textAlign: 'center', backgroundColor: '#ccc', height: '100%' } }>
-         <h1>抱歉，您访问的页面不存在。</h1><br/>
+      <div style={ { paddingTop: '200px', textAlign: 'center', backgroundColor: '#ccc', height: '100%' } }>
+         <h1>404</h1>
+         <h3>
+           抱歉，您访问的页面不存在。
+         </h3><br/>
          <h4>
            <font color='red' id='num'>3</font>
-           <span>&nbsp;秒钟后，自动跳转到项目中心页面。</span>
+           <span>&nbsp;秒钟后，自动跳转至登录页面。</span>
         </h4>
       </div>
     );
