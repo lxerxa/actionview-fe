c898730c2ed7d4e64ffa10425a6838432b0f32b1
lxerxa <lxerxa@126.com>
2020-04-18 12:30:40 +0800

    td first

--- app/components/issue/List.jsx
+++ app/components/issue/List.jsx
@@ -291,7 +291,7 @@ export default class List extends Component {
 
     if (this.state.barShow) {
       $('.react-bs-container-body table tr').each(function(i) {
-        if (itemData.id === $(this).find(':hidden').text()) {
+        if (itemData.id === $(this).find('td:first').text()) {
           $(this).css('background-color', '#eee');
         } else {
           $(this).css('background-color', '');
