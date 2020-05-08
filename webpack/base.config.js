8bac60f12592e35014bc833ee87f51ec625dbfac
lxerxa <lxerxa@126.com>
2020-05-08 21:37:40 +0800

    assets path

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -11,7 +11,7 @@ export default {
       path: path.resolve(__dirname, '../dist'),
       filename: '[name]-[hash].js',
       chunkFilename: '[name]-[hash].js',
-      publicPath: '/assets/'
+      publicPath: '/actionview/assets/'
     },
     module: {
       preLoaders: [
