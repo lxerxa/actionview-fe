bf4e4578d8b29d9c074603fee3d9f292ec38b11d
lxerxa <lxerxa@126.com>
2021-02-16 14:55:59 +0800

    duration

--- app/components/share/Duration.jsx
+++ app/components/share/Duration.jsx
@@ -10,7 +10,7 @@ export default class Duration extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      options: !_.isEmpty(props.options) ? props.options : [ 'fixed', 'current_duration', 'past_duration', 'variable_duration' ],
+      options: !_.isEmpty(props.options) ? props.options : [ 'fixed', 'current_duration', 'variable_duration' ],
       mode: 'fixed',
       start_time: '', 
       end_time: '', 
@@ -33,7 +33,7 @@ export default class Duration extends Component {
       return;
     }
 
-    let mode = this.state.mode, start_time = '', end_time = '', past_value = '', current_value = '', start_direct = this.state.start_direct, start_value = '', end_direct = this.state.end_direct, end_value = '';
+    let mode = this.state.mode, start_time = '', end_time = '', current_value = '', start_value = '', end_value = '';
 
     const suffix_list = [ 'd', 'w', 'm', 'y' ];
 
@@ -76,7 +76,7 @@ export default class Duration extends Component {
       current_value = sections[0]; 
     }
 
-    this.setState({ mode, start_time, end_time, current_value, start_direct, start_value, end_direct, end_value });
+    this.setState({ mode, start_time, end_time, current_value, start_value, end_value });
   }
 
   async onChange(data) {
@@ -94,7 +94,6 @@ export default class Duration extends Component {
       start_time, 
       end_time, 
       current_value,
-      past_value,
       start_value, 
       end_value 
     } = this.state;
@@ -107,8 +106,6 @@ export default class Duration extends Component {
       }
     } else if (mode === 'current_duration') {
       return current_value;
-    } else if (mode === 'past_duration') {
-      return past_value + '~';
     } else if (mode === 'variable_duration') {
       let tmp_start_value = '', tmp_end_value = '';
       if (start_value || start_value === 0) {
@@ -131,14 +128,11 @@ export default class Duration extends Component {
       start_time, 
       end_time, 
       current_value,
-      past_value,
-      start_direct, 
       start_value, 
-      end_direct, 
       end_value 
     } = this.state;
 
-    const { value, onChange } = this.props;
+    const { value='', onChange } = this.props;
 
     const modeOptions = [
       { value: 'fixed', label: '固定时间段' }, 
@@ -148,66 +142,9 @@ export default class Duration extends Component {
 
     const currentDurations = [
       { value: '0d', label: '当天' },
-      { value: '0w', label: '当前周' },
+      { value: '0w', label: '本周' },
       { value: '0m', label: '当月' },
       { value: '0y', label: '当前年' }
-    ];
-
-    const directOptions = [
-      { value: 'past', label: '前' },
-      { value: 'future', label: '后' }
-    ];
-
-    const variableDurations = [
-      { value: '0d', label: '当天' },
-      { value: '1d', label: '1天' },
-      { value: '2d', label: '2天' },
-      { value: '3d', label: '3天' },
-      { value: '4d', label: '4天' },
-      { value: '5d', label: '5天' },
-      { value: '6d', label: '6天' },
-      { value: '1w', label: '1周' },
-      { value: '2w', label: '2周' },
-      { value: '3w', label: '3周' },
-      { value: '1m', label: '1个月' },
-      { value: '2m', label: '2个月' },
-      { value: '3m', label: '3个月' },
-      { value: '4m', label: '4个月' },
-      { value: '5m', label: '5个月' },
-      { value: '6m', label: '6个月' },
-      { value: '7m', label: '7个月' },
-      { value: '8m', label: '8个月' },
-      { value: '9m', label: '9个月' },
-      { value: '10m', label: '10个月' },
-      { value: '11m', label: '11个月' },
-      { value: '1y', label: '1年' },
-      { value: '2y', label: '2年' }
-    ];
-
-    const pastVariableDurations = [
-      { value: '1w', label: '1周' },
-      { value: '2w', label: '2周' },
-      { value: '3w', label: '3周' },
-      { value: '1m', label: '1个月' },
-      { value: '2m', label: '2个月' },
-      { value: '3m', label: '3个月' },
-      { value: '4m', label: '4个月' },
-      { value: '5m', label: '5个月' },
-      { value: '6m', label: '6个月' },
-      { value: '7m', label: '7个月' },
-      { value: '8m', label: '8个月' },
-      { value: '9m', label: '9个月' },
-      { value: '10m', label: '10个月' },
-      { value: '11m', label: '11个月' },
-      { value: '1y', label: '1年' },
-      { value: '2y', label: '2年' }
-    ];
-
-    const currentVariableDurations = [
-      { value: 'd', label: '天' },
-      { value: 'w', label: '周' },
-      { value: 'm', label: '月' },
-      { value: 'y', label: '年' }
     ];
 
     return (
@@ -246,8 +183,8 @@ export default class Duration extends Component {
             type='number'
             value={ this.state.start_value }
             placeholder='请输入'
-            onBlur={ () => { const curValue = this.getValue(); if (value != curValue) { onChange(curValue) } } }
-            onChange={ (e) => { this.setState({ start_value: parseInt(e.target.value) }); } } />
+            onBlur={ async (e) => { await this.setState({ start_value: isNaN(parseInt(e.target.value)) ? '' : parseInt(e.target.value) }); const curValue = this.getValue(); if (value != curValue) { onChange(curValue) } } }
+            onChange={ (e) => { this.setState({ start_value: e.target.value }); } } />
         </div> }
         { mode === 'variable_duration' &&
         <div style={ { float: 'left', marginTop: '9px', paddingLeft: '3px' } }>
@@ -279,8 +216,8 @@ export default class Duration extends Component {
             type='number'
             value={ this.state.end_value }
             placeholder='请输入'
-            onBlur={ () => { const curValue = this.getValue(); if (value != curValue) { onChange(curValue) } } }
-            onChange={ (e) => { this.setState({ end_value: parseInt(e.target.value) }); } } />
+            onBlur={ async (e) => { await this.setState({ end_value: isNaN(parseInt(e.target.value)) ? '' : parseInt(e.target.value) }); const curValue = this.getValue(); if (value != curValue) { onChange(curValue) } } }
+            onChange={ (e) => { this.setState({ end_value: e.target.value }); } } />
         </div> }
         { mode === 'variable_duration' &&
         <div style={ { float: 'left', marginTop: '9px', paddingLeft: '3px' } }>
